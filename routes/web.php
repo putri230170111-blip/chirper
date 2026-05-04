7e9035b07631d60216bdac0920bb49fa346b2846
putri230170111-blip <putri.230170111@mhs.unimal.ac.id>
2026-05-04 22:47:46 +0700

    show data dummy

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirpController::Class,'index'] );
+     
+
